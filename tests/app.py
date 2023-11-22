eb20377dd5652e5379012f70db9607f85cac823b
Rishikesh Unawane <rishikesh.unawane20@vit.edu>
2023-11-22 18:46:57 +0530

    tests folder changes

--- tests/app.py
+++ tests/app.py
@@ -10,184 +10,190 @@ import folium
 from bson import ObjectId
 from geopy.geocoders import Nominatim
 
-app = Flask(__name__, static_folder='static')
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
-#Setting up Cloud DB
-# uri = "<MONGODB_URL>"
-# client = MongoClient(uri, server_api=ServerApi('1'))
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
 
-# Setting up Local DB
-client = MongoClient('localhost', 27017)
-db = client.AeroNav
-orders = db.orders
-users = db.users
+def create_app(config):
+    app = Flask(__name__, static_folder='static')
+    app.config.from_object(config)
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
 
-@app.route("/", methods=['GET', 'POST'])
-def sign_up():
-    message = ''
-    if 'username' in session:
-        return render_template('index.html', username=session['username'])
-    if request.method == "POST":
-        user = request.form.get('username')
-        password = request.form.get('password')
-        user_type = request.form.get('user_type')
-        address = request.form.get('address')
-        geolocator = Nominatim(user_agent="app")
-        location = geolocator.geocode(address)
-        user_found = users.find_one({"username": user})
-        if user_found:
-            message = 'There already is a user by that name'
-            return render_template('users/index.html', message=message)
-        else:
-            hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            user_input = {
-                    'username': user,
-                    'encrypt_pass': hashed, 
-                    'type': user_type, 
-                    'location' : address,
-                    'x_cord' : location.latitude,
-                    'y_cord' : location.longitude,
-                    "timestamp": datetime.utcnow()
-                    }
-            x = users.insert_one(user_input)
-            print(x)
-            return redirect('/login')
-    return render_template('users/index.html')
+    #Setting up Cloud DB
+    # uri = "<MONGODB_URL>"
+    # client = MongoClient(uri, server_api=ServerApi('1'))
+    # try:
+    #     client.admin.command('ping')
+    #     print("Pinged your deployment. You successfully connected to MongoDB!")
+    # except Exception as e:
+    #     print(e)
 
-# Signin route
-@app.route('/login', methods=['GET', 'POST'])
-def sign_in():
-    if request.method == 'POST':
-        user = users.find_one({'username': request.form['username']})
-        print(user['type'])
-        if user:
-            if bcrypt.checkpw(request.form['password'].encode('utf-8'), user['encrypt_pass']):
-                session['_id'] = user['_id']
-                print(session['_id'])
-                if user['type']=='Customer':
-                    return redirect('/customer/orders')
-                else:
-                    return redirect('/distributor/orders')
-                # return render_template('index.html')
-            return render_template('users/login.html', message='Invalid username/password combination')
-        return render_template('users/login.html', message='User not found!')
-    return render_template('users/login.html')
+    # Setting up Local DB
+    client = MongoClient('localhost', 27017)
+    db = client.AeroNav
+    orders = db.orders
+    users = db.users
 
-@app.route('/customer/orders',methods=['GET'])
-def view_orders():
-    all_orders = orders.find()
-    return render_template('customers/orders.html', orders=all_orders)
+    @app.route("/", methods=['GET', 'POST'])
+    def sign_up():
+        message = ''
+        if 'username' in session:
+            return render_template('index.html', username=session['username'])
+        if request.method == "POST":
+            user = request.form.get('username')
+            password = request.form.get('password')
+            user_type = request.form.get('user_type')
+            address = request.form.get('address')
+            geolocator = Nominatim(user_agent="app")
+            location = geolocator.geocode(address)
+            user_found = users.find_one({"username": user})
+            if user_found:
+                message = 'There already is a user by that name'
+                return render_template('users/index.html', message=message)
+            else:
+                hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                user_input = {
+                        'username': user,
+                        'encrypt_pass': hashed, 
+                        'type': user_type, 
+                        'location' : address,
+                        'x_cord' : location.latitude,
+                        'y_cord' : location.longitude,
+                        "timestamp": datetime.utcnow()
+                        }
+                x = users.insert_one(user_input)
+                print(x)
+                return redirect('/login')
+        return render_template('users/index.html')
 
-@app.route('/customer/orders/<id>',methods=['GET', 'POST'])
-def view_order(id):
-    order = orders.find_one({'_id':ObjectId(id)})
-    if request.method == 'POST':
-        customer_id = session['_id']
-        order['customers'].append(ObjectId(customer_id))
-        order = orders.find_one_and_replace({'_id':ObjectId(id)}, order, return_document=ReturnDocument.AFTER)
-    order['created_by'] = users.find_one({'_id':order['created_by']})
-    return render_template('customers/order.html', order=order)
+    # Signin route
+    @app.route('/login', methods=['GET', 'POST'])
+    def sign_in():
+        if request.method == 'POST':
+            user = users.find_one({'username': request.form['username']})
+            print(user['type'])
+            if user:
+                if bcrypt.checkpw(request.form['password'].encode('utf-8'), user['encrypt_pass']):
+                    session['_id'] = user['_id']
+                    print(session['_id'])
+                    if user['type']=='Customer':
+                        return redirect('/customer/orders')
+                    else:
+                        return redirect('/distributor/orders')
+                    # return render_template('index.html')
+                return render_template('users/login.html', message='Invalid username/password combination')
+            return render_template('users/login.html', message='User not found!')
+        return render_template('users/login.html')
 
-@app.route('/distributor/order',methods=['GET', 'POST'])
-def create_order():
-    if request.method == 'POST':
-        distributor_id = session['_id']
-        order = orders.insert_one({
-            'title' : request.form.get('title'),
-            'created_by' : distributor_id,
+    @app.route('/customer/orders',methods=['GET'])
+    def view_orders():
+        all_orders = orders.find()
+        return render_template('customers/orders.html', orders=all_orders)
+
+    @app.route('/customer/orders/<id>',methods=['GET', 'POST'])
+    def view_order(id):
+        order = orders.find_one({'_id':ObjectId(id)})
+        if request.method == 'POST':
+            customer_id = session['_id']
+            order['customers'].append(ObjectId(customer_id))
+            order = orders.find_one_and_replace({'_id':ObjectId(id)}, order, return_document=ReturnDocument.AFTER)
+        order['created_by'] = users.find_one({'_id':order['created_by']})
+        return render_template('customers/order.html', order=order)
+
+    @app.route('/distributor/order',methods=['GET', 'POST'])
+    def create_order():
+        if request.method == 'POST':
+            distributor_id = session['_id']
+            order = orders.insert_one({
+                'title' : request.form.get('title'),
+                'created_by' : distributor_id,
+                'customers' : [],
+                'is_optimized' : False, 
+                'optimized_path' : [],
+                'vehicles' : request.form.get('vehicles'),
+                "timestamp": datetime.utcnow()
+            }) 
+            return redirect('/distributor/orders/' + str(order.inserted_id))
+        return render_template('distributors/create_order.html')
+
+    @app.route('/distributor/orders',methods=['GET'])
+    def view_my_orders():
+        all_orders = orders.find({'created_by' : session['_id']})
+        return render_template('distributors/orders.html', orders=all_orders)
+
+    @app.route('/distributor/orders/<id>',methods=['GET', 'POST'])
+    def view_my_order(id):
+        order = orders.find_one({'_id':ObjectId(id)})
+        order['created_by'] = users.find_one({'_id':order['created_by']})
+        for i in range(0,len(order['customers'])):
+            order['customers'][i] = users.find_one({'_id':ObjectId(order['customers'][i])})
+
+        if request.method == 'POST' and request.args.get('_method') == 'DELETE':
+            orders.delete_one({'_id':ObjectId(id)})
+            return redirect('/distributor/orders')
+        
+        elif request.method == 'POST':
+            optimized_path = findOptimizedPath(order['customers'],order['created_by'],order['vehicles'])
+            orderCopy = orders.find_one({'_id':ObjectId(id)})
+            orderCopy['optimized_path'] = optimized_path
+            orderCopy['is_optimized'] = True
+            orders.find_one_and_replace({'_id':ObjectId(id)}, orderCopy, return_document=ReturnDocument.AFTER)
+            return redirect('/distributor/orders/'+id)
+        
+        return render_template('distributors/order.html', order=order)
+
+    @app.route('/distributor/orders/<id>/visualize',methods=['GET'])
+    def visualize_my_order(id):
+        order= orders.find_one({'_id':ObjectId(id)})
+        if not order['is_optimized']:
+            return redirect('/distributor/orders/'+id)
+        
+        map = folium.Map(location=order['optimized_path'][0][0], zoom_start=13)
+        colors = ['blue','red','green','orange','violet','black']
+        map_path = 'templates/map.html'
+        i=0
+        for vehicleRoute in order['optimized_path']:
+            route = folium.PolyLine(locations=vehicleRoute, color=colors[i])
+            i += 1
+            for each in vehicleRoute:
+                folium.Marker(each).add_to(map)
+            route.add_to(map)
+        map.save(map_path)
+        return render_template('map.html', map_path=map_path)
+
+    #Adds sample data to DB
+    @app.route('/populateDB', methods=["GET"])
+    def populateDB():
+        users.insert_one({
+            'username' : 'Jhon',
+            'encrypt_pass' : 'hello@1$',
+            'type' : 'customer',
+            'location' : 'Pune',
+            'x_cord' : 18.5204,
+            'y_cord' : 73.8567,
+            "timestamp": datetime.utcnow()
+        })
+        user = users.insert_one({
+            'username' : 'Mia',
+            'encrypt_pass' : 'hello@1$',
+            'type' : 'distributor',
+            'location' : 'Pune',
+            'x_cord' : 18.5204,
+            'y_cord' : 73.8567,
+            "timestamp": datetime.utcnow()
+        })
+        orders.insert_one({
+            'title' : 'My Fav Order',
+            'created_by' : user.inserted_id,
             'customers' : [],
             'is_optimized' : False, 
             'optimized_path' : [],
-            'vehicles' : request.form.get('vehicles'),
+            'vehicles' : 0,
             "timestamp": datetime.utcnow()
-        }) 
-        return redirect('/distributor/orders/' + str(order.inserted_id))
-    return render_template('distributors/create_order.html')
+        })  
+        return "DB updated with sample data!"
 
-@app.route('/distributor/orders',methods=['GET'])
-def view_my_orders():
-    all_orders = orders.find({'created_by' : session['_id']})
-    return render_template('distributors/orders.html', orders=all_orders)
-
-@app.route('/distributor/orders/<id>',methods=['GET', 'POST'])
-def view_my_order(id):
-    order = orders.find_one({'_id':ObjectId(id)})
-    order['created_by'] = users.find_one({'_id':order['created_by']})
-    for i in range(0,len(order['customers'])):
-        order['customers'][i] = users.find_one({'_id':ObjectId(order['customers'][i])})
-
-    if request.method == 'POST' and request.args.get('_method') == 'DELETE':
-        orders.delete_one({'_id':ObjectId(id)})
-        return redirect('/distributor/orders')
-    
-    elif request.method == 'POST':
-        optimized_path = findOptimizedPath(order['customers'],order['created_by'],order['vehicles'])
-        orderCopy = orders.find_one({'_id':ObjectId(id)})
-        orderCopy['optimized_path'] = optimized_path
-        orderCopy['is_optimized'] = True
-        orders.find_one_and_replace({'_id':ObjectId(id)}, orderCopy, return_document=ReturnDocument.AFTER)
-        return redirect('/distributor/orders/'+id)
-    
-    return render_template('distributors/order.html', order=order)
-
-@app.route('/distributor/orders/<id>/visualize',methods=['GET'])
-def visualize_my_order(id):
-    order= orders.find_one({'_id':ObjectId(id)})
-    if not order['is_optimized']:
-        return redirect('/distributor/orders/'+id)
-    
-    map = folium.Map(location=order['optimized_path'][0][0], zoom_start=13)
-    colors = ['blue','red','green','orange','violet','black']
-    map_path = 'templates/map.html'
-    i=0
-    for vehicleRoute in order['optimized_path']:
-        route = folium.PolyLine(locations=vehicleRoute, color=colors[i])
-        i += 1
-        for each in vehicleRoute:
-          folium.Marker(each).add_to(map)
-        route.add_to(map)
-    map.save(map_path)
-    return render_template('map.html', map_path=map_path)
-
-#Adds sample data to DB
-@app.route('/populateDB', methods=["GET"])
-def populateDB():
-    users.insert_one({
-        'username' : 'Jhon',
-        'encrypt_pass' : 'hello@1$',
-        'type' : 'customer',
-        'location' : 'Pune',
-        'x_cord' : 18.5204,
-        'y_cord' : 73.8567,
-        "timestamp": datetime.utcnow()
-    })
-    user = users.insert_one({
-        'username' : 'Mia',
-        'encrypt_pass' : 'hello@1$',
-        'type' : 'distributor',
-        'location' : 'Pune',
-        'x_cord' : 18.5204,
-        'y_cord' : 73.8567,
-        "timestamp": datetime.utcnow()
-    })
-    orders.insert_one({
-        'title' : 'My Fav Order',
-        'created_by' : user.inserted_id,
-        'customers' : [],
-        'is_optimized' : False, 
-        'optimized_path' : [],
-        'vehicles' : 0,
-        "timestamp": datetime.utcnow()
-    })  
-    return "DB updated with sample data!"
+    return app
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,18 @@
 import pytest
-from app import app
-
-@pytest.fixture()
-def app():
-    # app.config.update({
-    #     "TESTING": True,
-    # })
-
-    # other setup can go here
-
-    yield app
-
-    # clean up / reset resources here
+from app import create_app
 
 
-@pytest.fixture()
-def client(app):
-    return app.test_client()
+@pytest.fixture
+def client():
+    app = create_app({"TESTING": True})
+    with app.test_client() as client:
+        yield client
+
+# @pytest.fixture()
+# def client(app):
+#     return app.test_client()
 
 
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+# @pytest.fixture()
+# def runner(app):
+#     return app.test_cli_runner()

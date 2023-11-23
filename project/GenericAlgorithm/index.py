d2d73e53852f586d00d03fb463c31e28b726b52a
Mandar Patil <80695146+Patil-Mandar@users.noreply.github.com>
2023-11-23 15:05:27 +0530

    Path calculation (#4)
    * moved project back to root dir
    
    * Revert "moved project back to root dir"
    
    This reverts commit bf31a7fa4263f21b9fca6933ec145450a768c5f6.
    
    * added distance calculator for customer
    
    * added distance calculator for distributor
    
    * added velocity in create order form
    
    * copied chenges in testing f folder

--- project/GenericAlgorithm/index.py
+++ project/GenericAlgorithm/index.py
@@ -1,3 +1,4 @@
+from math import cos, asin, sqrt, pi
 from GenericAlgorithm.city import City
 from GenericAlgorithm.algorithm import geneticAlgorithm
 
@@ -25,3 +26,32 @@ def findOptimizedPath(customers,distributor,vehicles):
             routesCords.append(vehicleRoutesCords)
 
     return routesCords
+
+
+def distance(lat1, lon1, lat2, lon2):
+    r = 6371 # km
+    p = pi / 180
+
+    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
+    return 2 * r * asin(sqrt(a))
+
+def calculateDistance(routesCords):
+    drone = 1
+    data = []
+    for vehicleRoute in routesCords:
+      totalDistance = 0
+      for i in range(1,len(vehicleRoute)):
+        totalDistance += distance(vehicleRoute[i][0], vehicleRoute[i][1], vehicleRoute[i-1][0], vehicleRoute[i-1][1])
+      data.append(totalDistance)
+      drone += 1
+    return data
+
+def calculateDistanceForCustomer(routesCords, x, y):
+    for vehicleRoute in routesCords:
+      for cordinates in vehicleRoute:
+          if cordinates[0]==x and cordinates[1]==y:
+              totalDistance = 0
+              for i in range(1,len(vehicleRoute)):
+                  totalDistance += distance(vehicleRoute[i][0], vehicleRoute[i][1], vehicleRoute[i-1][0], vehicleRoute[i-1][1])
+                  if(vehicleRoute[i][0]==x and vehicleRoute[i][1]==y):
+                    return totalDistance
